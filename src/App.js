67774658e04f7d41665176f326df6b6aba7827a8
d3vNano <luizdorigo01@gmail.com>
2023-03-28 17:23:55 -0300

    Feat: basics, adjustments & news, footer basic imp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import HomePage from "./pages/home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <ToastContainer />
+            <Router>
+                <Routes>
+                    <Route path="/home" element={<HomePage />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import * as ss from "./css";
+
+function HomePage() {
+    return (
+        <>
+            <ss.Test>wee</ss.Test>
+        </>
+    );
+}
+
+export default HomePage;
